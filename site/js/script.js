af9bd990e9705d02be70ed0bbceb399f526d6dee
birkajensen <birkajensen@gmail.com>
2016-08-08 15:31:16 +0200

    facebook and twtter ok

--- site/js/script.js
+++ site/js/script.js
@@ -2,23 +2,13 @@
 // controlling the margins on menu moving it into the page if it is active
 
 $(function() {
-  $('.panel-heading').click(function() {
+  $('.panel').click(function() {
     if ($(this).hasClass('activestate')) {
       $(this).removeClass('activestate');
     } else {
-      $('.panel-heading').removeClass('activestate');
+      $('.panel').removeClass('activestate');
       $(this).addClass('activestate');
     }
   });
 });
 
-$(function() {
-  $('td').click(function() {
-    if ($(this).hasClass('activestate')) {
-      $(this).removeClass('activestate');
-    } else {
-      $('td').removeClass('activestate');
-      $(this).addClass('activestate');
-    }
-  });
-});
